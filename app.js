c7309cb5ad9ec6c781a8888df9582d50fcfad7b7
tributeTBC <134075185+tributeTBC@users.noreply.github.com>
2023-09-28 19:59:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,28 @@
 let web3;
-let userAccount;
+let web3Modal;
 
-const DAI_CONTRACT_ADDRESS = '0x6b175474e89094c44da98b954eedeac495271d0f';  // Example address, you should replace with your desired token's address.
-const DAI_ABI = [  // Abbreviated ABI. Only includes necessary parts for fetching balance.
-    {
-        "constant": true,
-        "inputs": [
-            {
-                "name": "_owner",
-                "type": "address"
-            }
-        ],
-        "name": "balanceOf",
-        "outputs": [
-            {
-                "name": "balance",
-                "type": "uint256"
-            }
-        ],
-        "type": "function"
-    }
-];
-function connectWallet() {
-    if (typeof window.ethereum !== 'undefined') {
-        web3 = new Web3(window.ethereum);
-        window.ethereum.enable().then(accounts => {
-            fetchNetwork();
-        });
-    } else {
-        alert('MetaMask not detected. Please install it first.');
-    }
-}
+async function initWeb3Modal() {
+    const providerOptions = {
+        // You can add other providers here if needed
+    };
 
-function fetchNetwork() {
-    web3.eth.net.getNetworkType().then(networkType => {
-        document.getElementById('networkName').textContent = networkType.charAt(0).toUpperCase() + networkType.slice(1); // Capitalize the first letter
+    web3Modal = new Web3Modal({
+        network: "mainnet",
+        cacheProvider: true, 
+        providerOptions
     });
 }
 
-
-function fetchDAIBalance() {
-    if (web3 && userAccount) {
-        const daiToken = new web3.eth.Contract(DAI_ABI, DAI_CONTRACT_ADDRESS);
-        daiToken.methods.balanceOf(userAccount).call().then(balance => {
-            document.getElementById('daiBalance').textContent = web3.utils.fromWei(balance) + ' DAI';
-        });
-    }
+async function connect() {
+    const provider = await web3Modal.connect();
+    web3 = new Web3(provider);
+    updateUI();
 }
+
+async function updateUI() {
+    const accounts = await web3.eth.getAccounts();
+    document.getElementById("account").innerText = `Connected account: ${accounts[0]}`;
+}
+
+// Initialize web3Modal
+initWeb3Modal();
